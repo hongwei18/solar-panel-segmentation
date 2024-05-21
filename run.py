f92203a0a007ec8a53a112f446fdf614396c3440
hongwei18 <168401696+hongwei18@users.noreply.github.com>
2024-05-21 15:14:31 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+from msilib.schema import Environment
+from venv import create
 import fire
 from solarnet.run import RunTask
 
 
 if __name__ == '__main__':
     fire.Fire(RunTask)
+
+conda venv create -f Environment.ubuntu.cpu.yml
+conda activate <environment_name>
